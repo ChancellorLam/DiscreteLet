abfb829e071ce850d71a26b8cd80fd814292c4a5
Owen Holm <owenholm87@gmail.com>
2025-09-06 21:38:53 -0400

    added routing pt. 2
    - finished adding routing for remaining components
    -added styling changes to home page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { Logicandproofs } from './pages/logicandproofs/logicandproofs';
 import { Numtheoryxcrypt } from './pages/numtheoryxcrypt/numtheoryxcrypt';
 import { Graphtheory } from './pages/graphtheory/graphtheory';
 import { Counting } from './pages/counting/counting';
+import { Basicstructures } from './pages/basicstructures/basicstructures';
+import { Relations } from './pages/relations/relations';
 
 export const routes: Routes = [ 
     { path: '', component: Logicandproofs},
     { path: '', component: Numtheoryxcrypt},
     { path: '', component: Graphtheory},
     { path: '', component: Counting},
+    { path: '', component: Basicstructures},
+    { path: '', component: Relations}
 ];
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,35 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  template: `
+    <h1>Welcome to DiscreteLet!</h1>
+    <nav>
+      <a routerLink="/logicandproofs">Logic and Proofs</a>
+      <a routerLink="/numtheoryxcrypt">Number Theory and Cryptography</a>
+      <a routerLink="/graphtheory">Graph Theory</a>
+      <a routerLink="/counting">Counting</a>
+      <a routerLink="/basicstructures">Basic Structures</a>
+      <a routerLink="/relations">Relations</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+
 })
 export class App {
   protected readonly title = signal('DiscreteLet');
-}
+   private activatedRoute = inject(ActivatedRoute);
+
+constructor() {
+    console.log(this.activatedRoute);
+  }
+
+
+  }
+
+
+
