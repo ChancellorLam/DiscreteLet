e68712c7746fb5bda8fa87581e395dfd3909ce70
Chancellor Lam <l2.chancellor@gmail.com>
2025-09-09 13:02:39 -0400

    Change changeDetection to OnPush in PrimengTestPage

--- client/src/app/pages/primeng-test-page/primeng-test-page.ts
+++ client/src/app/pages/primeng-test-page/primeng-test-page.ts
@@ -1,11 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-primeng-test-page',
   imports: [ButtonModule],
   templateUrl: './primeng-test-page.html',
-  styleUrl: './primeng-test-page.css'
+  styleUrl: './primeng-test-page.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PrimengTestPage {
   counter = signal(0);
