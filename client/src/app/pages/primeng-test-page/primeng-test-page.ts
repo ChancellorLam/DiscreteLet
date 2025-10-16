57202e9b6c61e3bed5fcb42679a575f8985fee59
Chancellor Lam <l2.chancellor@gmail.com>
2025-10-16 09:18:38 -0400

    Demo work in progress

--- client/src/app/pages/primeng-test-page/primeng-test-page.ts
+++ client/src/app/pages/primeng-test-page/primeng-test-page.ts
@@ -1,17 +1,40 @@
-import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
+import { Component, signal, effect, inject, ChangeDetectionStrategy, ViewChild, ElementRef } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import katex from 'katex';
+import { LogicalExpressionService } from '../../core/services/logical-expression-service';
 
 @Component({
   selector: 'app-primeng-test-page',
-  imports: [ButtonModule],
+  imports: [ButtonModule, InputTextModule, FormsModule],
   templateUrl: './primeng-test-page.html',
   styleUrl: './primeng-test-page.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PrimengTestPage {
-  counter = signal(0);
+  logicalExpressionService = inject(LogicalExpressionService);
+  @ViewChild('output', { static: true }) output!: ElementRef;
+  expression = signal<string>('\\lnot(A \\land B)');
+  subExpressions = signal<string[]>([]);
+  showSubExpression = signal<boolean>(false);
 
-  incrementCounter(): void {
-    this.counter.set(this.counter() + 1);
+  // renderMathEffect = effect(() => {
+  //   this.renderMath(this.expression());
+  // });
+
+  computeSubExpressions() {
+    console.log(this.expression());
+    console.log(this.logicalExpressionService.getSubExpressions(this.expression()));
   }
+
+  // renderMath(input: string): void {
+  //   try {
+  //     katex.render(input, this.output.nativeElement, {
+  //       throwOnError: false,
+  //     });
+  //   } catch (e) {
+  //     this.output.nativeElement.textContent = 'Rendering error';
+  //   }
+  // }
 }
