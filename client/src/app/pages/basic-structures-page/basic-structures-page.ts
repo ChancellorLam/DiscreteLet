264e6f49a6d437f40f568b4ba0a4547eaa4659da
griff <griffingulden1@gmail.com>
2025-10-28 14:25:38 -0400

    Basic Structures Page
    + information on sets
    + information on cardinality
    + basic set checker for set tab, soon to be component

--- client/src/app/pages/basic-structures-page/basic-structures-page.ts
+++ client/src/app/pages/basic-structures-page/basic-structures-page.ts
@@ -1,18 +1,61 @@
 import { Component } from '@angular/core';
 import { AccordionModule } from 'primeng/accordion';
 import { ToggleButtonModule } from 'primeng/togglebutton';
-import { FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { ButtonModule} from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-basic-structures-page',
-  imports: [AccordionModule, ToggleButtonModule, FormsModule, ButtonModule],
+  imports: [AccordionModule, ToggleButtonModule, FormsModule, ButtonModule, InputTextModule, CommonModule],
   templateUrl: './basic-structures-page.html',
   styleUrl: './basic-structures-page.css'
 })
 export class BasicStructuresPage {
   activeTabs: string[] = [];
   isExpanded = false;
+  value = '';
+  isValid = false;
+  parsedSet: string[] = [];
+
+checkSetInput() {
+  let input = this.value.trim();
+  if (!input) {
+    this.isValid = false;
+    this.parsedSet = [];
+    return;
+  } else if (input.startsWith('{') || input.endsWith('}')) {
+    input = input.slice(1, -1).trim();
+  }
+  
+
+  const pattern = /^[A-Za-z0-9]+(\s*,\s*[A-Za-z0-9]+)*$/;
+
+  if (!pattern.test(input)) {
+    this.isValid = false;
+    this.parsedSet = [];
+    return;
+  }
+
+  const elements = input
+    .split(',')
+    .map(e => e.trim())
+    .filter(e => e !== '');
+
+    
+  const uniqueElements = new Set(elements);
+  if (elements.length !== uniqueElements.size) {
+    this.isValid = false;
+    this.parsedSet = [];
+    return;
+  }
+
+  this.isValid = true;
+  this.parsedSet = Array.from(uniqueElements);
+}
+
+
 
   toggleAll() {
     if (this.isExpanded) {
