ec531e39a89bf45e9ad803447a79847227440639
Chancellor Lam <l2.chancellor@gmail.com>
2025-11-18 07:37:13 -0500

    Change TruthTable component changeDetection to OnPush

--- client/src/app/pages/logic-page/truth-table/truth-table.ts
+++ client/src/app/pages/logic-page/truth-table/truth-table.ts
@@ -1,11 +1,12 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ChangeDetectionStrategy} from '@angular/core';
 import { LatexContainer } from '../../../shared/components/latex-container/latex-container';
 
 @Component({
   selector: 'app-truth-table',
   imports: [LatexContainer],
   templateUrl: './truth-table.html',
-  styleUrl: './truth-table.css'
+  styleUrl: './truth-table.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TruthTable {
   subexpressions = input<string[]>([]);
