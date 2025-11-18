098bffd0248ab2ba3d1022e01c2ba85dd3604bce
Chancellor Lam <l2.chancellor@gmail.com>
2025-11-18 07:32:02 -0500

    Create TruthTable component to display truth tables

--- client/src/app/pages/logic-page/truth-table/truth-table.ts
+++ client/src/app/pages/logic-page/truth-table/truth-table.ts
@@ -1,12 +1,12 @@
-import { Component, input, ChangeDetectionStrategy} from '@angular/core';
-import { LatexContainer } from '../../../shared/components/latex-container/latex-container';
+import { Component, computed, inject, input } from '@angular/core';
+import {LogicalExpressionService} from '../../../core/services/logical-expression-service';
+import {LatexContainer} from '../../../shared/components/latex-container/latex-container';
 
 @Component({
   selector: 'app-truth-table',
   imports: [LatexContainer],
   templateUrl: './truth-table.html',
-  styleUrl: './truth-table.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrl: './truth-table.css'
 })
 export class TruthTable {
   subexpressions = input<string[]>([]);
