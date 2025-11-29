61480427ee5a1e16d5cf5d62d4f10ff4fac90086
Owen Holm <owenholm87@gmail.com>
2025-11-29 16:49:58 -0500

    Login page
    -Created login page for Firebase
    -Will allow user to sign in with Google.
    - **This is still in progress**

--- server/src/routes/auth.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { verifyAndSyncUser, getCurrentUser } from '../controllers/auth.controller';
-
-const router = Router();
-
-router.post('/verify', verifyAndSyncUser);
-router.get('/me', getCurrentUser);
-
-export default router;
